ef8939dde8e2a2f8e1f0b13010d990e9c458efc8
Alon Zuman <alonzuman7@gmail.com>
2020-09-29 01:00:18 +0300

    m

--- src/containers/lists/ReviewsList.js
+++ src/containers/lists/ReviewsList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReviewSkeleton from '../../components/cards/ReviewSkeleton'
 import Review from '../../components/cards/Review'
 import './ReviewsList.css'
@@ -8,12 +8,37 @@ import { Skeleton } from '@material-ui/lab'
 import NoReviews from './NoReviews'
 
 const ReviewsList = ({ addReview, professor, reviews, loading }) => {
+  const [width, setWidth] = useState()
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth)
+    }
+    console.log(width)
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return (
-    <div className='page__section'>
-      <Typography variant='subtitle1'>{!loading ? reviews?.length > 0 ? heb.reviews : '' : <Skeleton width={120} />}</Typography>
+    <div className='page__section carousel__fix'>
+      <div className='flex align__center justify__between'>
+        <Typography variant='subtitle1'>{!loading ? reviews?.length > 0 ? heb.reviews : '' : <Skeleton width={120} />}</Typography>
+        {reviews && <Button className='small__btn ml-2' color='primary' onClick={() => setShow(!show)}>{loading ? <Skeleton width={80} /> : show ? heb.hide : heb.showAll}</Button>}
+        {loading && <Button className='small__btn ml-2' color='primary'><Skeleton width={80} /></Button>}
+      </div>
       <div className='reviews_list__container'>
         {loading && [0, 0, 0, 0]?.map((v, i) => <ReviewSkeleton key={i} />)}
-        {!loading && reviews?.map((review, index) => <Review professor={professor} review={review} key={index} />)}
+        {!loading && reviews?.map((review, i) => {
+          if (width <= 768) {
+            return <Review professor={professor} review={review} key={i} />
+          } else if (show) {
+            return <Review professor={professor} review={review} key={i} />
+          } else if (i <= 3) {
+            return <Review professor={professor} review={review} key={i} />
+          }
+        })}
         {!loading && reviews?.length === 0 && <NoReviews handleClick={addReview} />}
       </div>
       {!loading && reviews?.length !== 0 && <Button variant='contained' className='mt-2' color='primary' onClick={addReview}>{heb.addReview}</Button>}
