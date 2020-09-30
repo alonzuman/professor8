1679fd3299b9d13932ef9dfa972f84e2d2ed2205
Alon Zuman <alonzuman7@gmail.com>
2020-09-30 16:04:15 +0300

    hot fix

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,10 @@
-import { Typography } from '@material-ui/core'
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import qs from 'query-string'
-import heb from '../../utils/translation/heb'
 import { validateStringInput } from '../../utils/form'
 import HeroSvg from '../../assets/svgs/Students'
 import HomeSearchBar from './components/HomeSearchBar'
 import './Home.css'
-import Certification from '../../assets/svgs/Certification'
-import NoResultsFound from '../../assets/svgs/NoResultsFound'
-import Maths from '../../assets/svgs/Maths'
-import Trends from '../../assets/svgs/Trends'
 
 const Home = () => {
   const history = useHistory()
@@ -39,7 +33,6 @@ const Home = () => {
   return (
     <div className='home_content__container' dir='rtl'>
       <HeroSvg className='hero_svg' />
-      <Trends className='bottom_right__svg' />
       <HomeSearchBar
         loading={loading}
         handleSubmit={handleSubmit}
